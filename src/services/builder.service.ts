46b57afcfe7a68693037f14a56f5d1a571dce312
j0nl1 <jonlitech@gmail.com>
2022-10-03 05:19:13 -0500

    feat: support one schema generation

--- src/services/builder.service.ts
+++ src/services/builder.service.ts
@@ -11,6 +11,11 @@ class BuilderService {
   static getSchema(cwd: string, repoPath: string): Record<string, unknown> {
     const schemaPath = path.join(cwd, 'cw', repoPath, 'schema');
     const files = readdirSync(schemaPath);
+    if (files.length === 1) {
+      const schema = require(path.join(schemaPath, files[0]));
+      const { instantiate, execute, query } = schema;
+      return { instantiateMsg: instantiate, executeMsg: execute, queryMsg: query };
+    }
     if (!['instantiate_msg.json', 'execute_msg.json', 'query_msg.json'].every((nameFile) => files.includes(nameFile))) throw new HttpError(501);
     const instantiateMsg = require(path.join(schemaPath, 'instantiate_msg.json'));
     const executeMsg = require(path.join(schemaPath, 'execute_msg.json'));
